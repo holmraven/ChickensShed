91dc670b3c5572b65f30b10b27495e0ceeb1c582
holmraven <16858119+holmraven@users.noreply.github.com>
2022-06-29 04:26:57 +0300

    Fix server crash

--- src/main/java/com/holmraven/chickensshed/config/ConfigHandler.java
+++ src/main/java/com/holmraven/chickensshed/config/ConfigHandler.java
@@ -2,40 +2,23 @@ package com.holmraven.chickensshed.config;
 
 import com.holmraven.chickensshed.ChickensShed;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import org.apache.commons.lang3.tuple.Pair;
 
 @Mod.EventBusSubscriber(modid = ChickensShed.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ConfigHandler {
-    public static final ClientConfig CLIENT;
-    public static final ForgeConfigSpec CLIENT_SPEC;
+    public static final Common COMMON;
+    public static final ForgeConfigSpec COMMON_SPEC;
     static {
-        final Pair<ClientConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
-        CLIENT_SPEC = specPair.getRight();
-        CLIENT = specPair.getLeft();
+        final Pair<Common, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Common::new);
+        COMMON_SPEC = specPair.getRight();
+        COMMON = specPair.getLeft();
     }
 
-    public static boolean chicksDropFeathers;
-    public static int dropFrequency;
-
-    @SubscribeEvent
-    public static void onModConfigEvent(final ModConfig.ModConfigEvent configEvent) {
-        if(configEvent.getConfig().getSpec() == ConfigHandler.CLIENT_SPEC) {
-            bakeConfig();
-        }
-    }
-
-    public static void bakeConfig() {
-        chicksDropFeathers = CLIENT.chicksDropFeathers.get();
-        dropFrequency = CLIENT.dropFrequency.get();
-    }
-
-    public static class ClientConfig {
+    public static class Common {
         public final ForgeConfigSpec.BooleanValue chicksDropFeathers;
         public final ForgeConfigSpec.IntValue dropFrequency;
-        public ClientConfig(ForgeConfigSpec.Builder builder) {
+        public Common(ForgeConfigSpec.Builder builder) {
             builder.push("general");
             chicksDropFeathers = builder
                     .comment("Do baby chickens drop feathers?")
