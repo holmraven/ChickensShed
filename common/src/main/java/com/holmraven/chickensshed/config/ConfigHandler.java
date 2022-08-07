a589fe73fdc140d28f0d28defa10137208dfd025
holmraven <16858119+holmraven@users.noreply.github.com>
2022-08-07 16:37:41 +0300

    Change config tooltips implementation

--- common/src/main/java/com/holmraven/chickensshed/config/ConfigHandler.java
+++ common/src/main/java/com/holmraven/chickensshed/config/ConfigHandler.java
@@ -7,10 +7,12 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = ChickensShed.MODID)
+@Config.Gui.Background("minecraft:textures/block/white_wool.png")
 public class ConfigHandler implements ConfigData {
-    @Comment("Do baby chickens drop feathers?")
+    @ConfigEntry.Gui.Tooltip
     public boolean chicksDropFeathers = true;
-    @Comment("How often will feathers be shed? (min: 6000, less is more often)")
+
+    @ConfigEntry.Gui.Tooltip
     @ConfigEntry.BoundedDiscrete(min = 6000, max = Integer.MAX_VALUE)
     public int dropFrequency = 26000;
 }
