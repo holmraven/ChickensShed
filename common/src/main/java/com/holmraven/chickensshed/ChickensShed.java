57056ebe1c1eca613b8c57d1f7ad17a689e142d8
holmraven <16858119+holmraven@users.noreply.github.com>
2022-08-07 16:40:46 +0300

    Compatibility with Supplementaries

--- common/src/main/java/com/holmraven/chickensshed/ChickensShed.java
+++ common/src/main/java/com/holmraven/chickensshed/ChickensShed.java
@@ -2,8 +2,7 @@ package com.holmraven.chickensshed;
 
 import com.holmraven.chickensshed.config.ConfigHandler;
 import me.shedaniel.autoconfig.AutoConfig;
-import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Items;
 
 public class ChickensShed {
@@ -15,13 +14,12 @@ public class ChickensShed {
         ChickensShed.CONFIG = AutoConfig.getConfigHolder(ConfigHandler.class).getConfig();
     }
 
-    public static void performShedding(Animal animal) {
-        Chicken chicken = (Chicken) animal;
-        if (chicken.isBaby() && !CONFIG.chicksDropFeathers) {
+    public static void performShedding(LivingEntity e) {
+        if (e.isBaby() && !CONFIG.chicksDropFeathers) {
             return;
         }
-        if (!chicken.level.isClientSide && chicken.isAlive() && !chicken.isChickenJockey() && chicken.level.random.nextInt(CONFIG.dropFrequency) == 0) {
-            chicken.spawnAtLocation(Items.FEATHER);
+        if (!e.level.isClientSide && e.level.random.nextInt(CONFIG.dropFrequency) == 0) {
+            e.spawnAtLocation(Items.FEATHER);
         }
     }
 }
---
+++ forge/src/main/java/com/holmraven/chickensshed/integration/forge/CompatHandlerImpl.java
@@ -0,0 +1,9 @@
+package com.holmraven.chickensshed.integration.forge;
+
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class CompatHandlerImpl {
+    public static boolean isModLoaded(String name) {
+        return FMLLoader.getLoadingModList().getModFileById(name) != null;
+    }
+}
