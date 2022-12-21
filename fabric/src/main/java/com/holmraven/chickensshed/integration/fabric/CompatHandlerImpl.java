e2468ab9c0c557f1c3bb075db78c5343acd0b12c
holmraven <16858119+holmraven@users.noreply.github.com>
2022-12-21 21:57:32 +0200

    Removed Supplementaries compatibility

--- fabric/src/main/java/com/holmraven/chickensshed/integration/fabric/CompatHandlerImpl.java
+++
@@ -1,9 +0,0 @@
-package com.holmraven.chickensshed.integration.fabric;
-
-import net.fabricmc.loader.api.FabricLoader;
-
-public class CompatHandlerImpl {
-    public static boolean isModLoaded(String name) {
-        return FabricLoader.getInstance().isModLoaded(name);
-    }
-}
